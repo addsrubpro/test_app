9a0fc96dc2148a2a842440f8e3650bf170ad44dc
AB Samsung <a@.(none)>
2011-02-09 16:20:27 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'f9290800385b51005e1db2876eb1f9e150ace7bb94ac6397c3e441d0616037fa16d9bd2a80142b7484e4ab82983932cf9f4b58258d003f358b8879a358d57f10'
